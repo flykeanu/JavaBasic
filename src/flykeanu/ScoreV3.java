851abb72aaa9374527c32fe2d77f82bc8c25ed39
송준호 <flykeanu@naver.com>
2020-11-17 12:33:38 +0900

    55555

---
+++ src/flykeanu/ScoreV3.java
@@ -0,0 +1,106 @@
+package flykeanu;
+
+ import java.util.Scanner;
+
+/**
+  파일명 : ScoreV1
+ 작성일 : 성적처리 프로그램 V1
+ 이름 , 국어, 영어, 수학점수를 키보드로 입력받아
+ 총점, 평군, 학점을 계산하고 결과 출력
+단, 평균은 소수점 첫째 자리까지 출력하고
+학점기준은 '수우미양가'로 하되, switch 문으로 작성하시오
+
+
+ ***삼항연산자
+ (조건식)  ?  : (참일때 결과값)  :  (거짓일때 결과값)
+
+
+ */
+
+public class ScoreV3 {
+
+    public static void main(String[] args) {
+
+        String name = "송준호";
+        Scanner scanner = new Scanner(System.in);
+
+        int eng = scanner.nextInt();
+        int math = scanner.nextInt();
+        int kor = scanner.nextInt();
+        int sum = 0;
+        double avg = 0.0;
+        char grd = '가';
+
+
+        sum = eng + math + kor;
+
+        avg = (double)sum / 3;
+
+        System.out.println("이름 : " + name);
+        System.out.println("영어 : " + eng);
+        System.out.println("수학 : " + math);
+        System.out.println("국어 : " + kor);
+        System.out.println("합계 : " + sum);
+        System.out.println("과목평균 : " + avg);
+
+
+        switch ((int) (avg / 10)) {
+            case 10:
+                grd = '수';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 9:
+                grd = '수';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 8:
+                grd = '우';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+            case 7:
+                grd = '미';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+            case 6:
+                grd = '양';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+            case 5:
+                grd = '가';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 4:
+                grd = '가';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 3:
+                grd = '가';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 2:
+                grd = '가';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 1:
+                grd = '가';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+
+            case 0:
+                grd = '가';
+                System.out.println("평어는 : " + grd + "입니다.");
+                break;
+        }
+
+    }
+}
+
+
+
+
